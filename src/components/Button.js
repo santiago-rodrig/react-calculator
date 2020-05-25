83e0c621df1957dec3a72e31f276d3dc01f4289e
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-25 17:00:54 -0400

    create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+  return <div>{props.name}</div>;
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+}
