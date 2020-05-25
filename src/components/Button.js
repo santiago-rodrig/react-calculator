7e0a933f747988851a98c69c9599b1d29ff44dfe
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-25 18:45:20 -0400

    use a button tag for the Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button(props) {
-  return <div>{props.name}</div>;
+  return <button type="button">{props.name}</button>;
 }
 
 Button.propTypes = {
