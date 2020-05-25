ea551c36cb78ef8d2833897ec26ca77fa5a87e8c
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-25 17:00:37 -0400

    fix indentation of Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Display(props) {
-    return (
-        <div>{props.calculation}</div>
-    );
+  return (
+    <div>{props.calculation}</div>
+  );
 }
 
 Display.propTypes = {
-    calculation: PropTypes.string.isRequired,
+  calculation: PropTypes.string.isRequired,
 };
 
 Display.defaultProps = {
-    calculation: '0',
+  calculation: '0',
 };
 
 export default Display;
