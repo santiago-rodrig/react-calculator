2b49894047ec9d2073c9b82aae667d054a1dd6e5
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-26 07:01:45 -0400

    refactor rendering of ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,48 @@
 import React from 'react';
 import Button from './Button';
 
+const buttonsProps = [
+  { key: 1, name: 'AC' },
+  { key: 2, name: '+/-' },
+  { key: 3, name: '%' },
+  { key: 4, name: '÷' },
+  { key: 5, name: '7' },
+  { key: 6, name: '8' },
+  { key: 7, name: '9' },
+  { key: 8, name: 'X' },
+  { key: 9, name: '4' },
+  { key: 10, name: '5' },
+  { key: 11, name: '6' },
+  { key: 12, name: '-' },
+  { key: 13, name: '1' },
+  { key: 14, name: '2' },
+  { key: 15, name: '3' },
+  { key: 16, name: '+' },
+  { key: 17, name: '0' },
+  { key: 18, name: '.' },
+  { key: 19, name: '=' },
+];
+
+const buttonsElements = buttonsProps.map(prop => (
+  <Button key={prop.key} name={prop.name} />
+));
+
+const buttonGroupsProps = [
+  { key: 1 },
+  { key: 2 },
+  { key: 3 },
+  { key: 4 },
+  { key: 5 },
+];
+
+const buttonGroupsElements = buttonGroupsProps.map((prop, i) => (
+  <div key={prop.key}>{buttonsElements.slice((i * 4), ((i * 4) + 4))}</div>
+));
+
 function ButtonPanel() {
   return (
     <>
-      <div id="group-1">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
-        <Button name="÷" />
-      </div>
-      <div id="group-2">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="X" />
-      </div>
-      <div id="group-3">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" />
-      </div>
-      <div id="group-4">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" />
-      </div>
-      <div id="group-5">
-        <Button name="0" />
-        <Button name="." />
-        <Button name="=" />
-      </div>
+      {buttonGroupsElements}
     </>
   );
 }
