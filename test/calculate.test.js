a047f4dfa7f910435f69d31da361dc71ffdf5d22
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-26 16:37:27 -0400

    refactor: tests for calculate module

--- test/calculate.test.js
+++ test/calculate.test.js
@@ -1,14 +1,19 @@
 import Big from 'big.js/big';
 import calculate from '../src/logic/calculate';
+import operate from '../src/logic/operate';
 
-test('calculates the sum of two numbers', () => {
-  const calculator = { total: '1', next: '2', operation: null };
+function buildCalculator(total, next) {
+  return { total, next, operation: null };
+}
+
+function buildExpected(total, next, operation) {
+  const result = operate(total, next, operation);
+  return { total: result, next: null, operation: null };
+}
+
+test('calculates the sum of two common numbers', () => {
+  const calculator = buildCalculator('1', '2');
   const given = calculate(calculator, '+');
-  const expected = (() => {
-    const n1 = Big('1');
-    const n2 = Big('2');
-    return n1.plus(n2).toString();
-  })();
-  const { total } = given;
-  expect(total).toBe(expected);
+  const expected = buildExpected('1', '2', '+');
+  expect(given).toEqual(expected);
 });
