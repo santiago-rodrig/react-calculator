2371f83b3e7100e41089b730d630ca7b36bd293b
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-28 11:17:37 -0400

    feature: tests for calculate passing

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -31,6 +31,49 @@ function calculate(calculatorObj, buttonName) {
       }
 
       break;
+    case 'AC':
+      if (next) {
+        next = '0';
+      } else if (operation) {
+        operation = undefined;
+      } else {
+        total = '0';
+      }
+
+      break;
+    case '.':
+      if (next) {
+        if (!next.includes('.')) {
+          next += '.';
+        }
+      } else if (operation) {
+        next = '0.';
+      } else if (!total.includes('.')) {
+        total += '.';
+      }
+
+      break;
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      if (next) {
+        next += buttonName;
+      } else if (operation) {
+        next = buttonName;
+      } else if (total === '0') {
+        total = buttonName;
+      } else {
+        total += buttonName;
+      }
+
+      break;
     default:
       total = operate(total, next, operation);
       next = undefined;
