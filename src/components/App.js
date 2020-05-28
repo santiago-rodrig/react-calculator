67b4cbb26687349ea7bc073d3affde35be47e730
Santiago Rodriguez <santo1996.29@gmail.com>
2020-05-28 12:08:25 -0400

    feature: App component has state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 function App() {
+  const [total, setTotal] = React.useState(undefined);
+  const [next, setNext] = React.useState(undefined);
+  const [operation, setOperation] = React.useState(undefined);
+
   return (
     <div id="calculator">
       <Display />
